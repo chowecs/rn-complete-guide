db47f5b1952826f47170eebc198c14cb54b19cef
Eddy <cschow22-c@my.cityu.edu.hk>
2022-07-30 12:30:33 +0800

    item flatlist

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList} from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -11,7 +11,7 @@ export default function App() {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal}]);
   };
 
   return (
@@ -25,9 +25,14 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-      <View>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
-      </View>
+      <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals} 
+        renderItem={itemData => 
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>}/>
+
     </View>
   );
 }
@@ -41,5 +46,13 @@ const styles = StyleSheet.create({
     flexDirection:'row', justifyContent:'space-between', alignItems:'center'
   },
   
-  input: {width:'80%', borderColor:'black', borderWidth:1, padding:10}
+  input: {width:'80%', borderColor:'black', borderWidth:1, padding:10},
+
+  listItem: {
+    padding:10,
+    marginVertical:10,
+    backgroundColor:'#ccc',
+    borderColor:'black',
+    borderWidth:1
+  }
 });
